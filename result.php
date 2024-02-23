244787952cb1cfe66a4ad070534fdc8d4882f1ab
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-23 15:34:38 +0100

    feat: censor input word in paragraph + get new paragraph length

--- result.php
+++ result.php
@@ -8,8 +8,14 @@ var_dump($paragraph);
 
 // lunghezza paragrafo
 $p_length= strlen($paragraph);
-// var_dump($p_length);
+var_dump($p_length);
 
+// censura al paragrafo 
+$censored_paragraph = str_replace($censored, '***', $paragraph);
+var_dump($censored_paragraph);
 
+// lunghezza paragrafo censurato 
+$cp_length= strlen($censored_paragraph);
+var_dump($cp_length);
 
 ?>
